280ac5c3cea08faa4dfc42fd1b03ee44d2bec579
KrishnenduSinha12 <56547419+KrishnenduSinha12@users.noreply.github.com>
2021-02-02 12:11:21 +0530

    Add files via upload

---
+++ get_data.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+f=open("HB2.dat","r")
+p=f.readlines()
+f.close()
+
+print("set sphere_scale=0.35")
+print("set sphere_color=yellow")
+print("set cartoon_color=violetpurple")
+print("set cartoon_transparency, 0.75")
+print("bg_color white")
+print("set ray_opaque_background, off")
+print("set defer_builds_mode, 4")
+
+
+print("\n")
+
+for i in range (1,len(p)):
+    if float(p[i].split()[6]) > 20:
+        if float(p[i].split()[5]) > 0:
+            print ("select a," + " " + "resid" + " " + str(p[i].split()[1][3:6])+ " " +" and " +"name CA")
+            print ("select b," + " " + "resid" + " " + str(p[i].split()[2][3:6])+ " " +" and " +"name CA")
+            print ("show spheres, a")
+            print ("show spheres, b_")
+            print ("label a , oneletter+resi")
+            print ("label b_, oneletter+resi")
+            print("distance"+ " d1 , a, b_")
+            print("hide labels," " d1")
+            print("set dash_color, red, d1")
+        else:
+            print ("select a," + " " + "resid" + " " + str(p[i].split()[1][3:6])+ " " +" and " +"name CA")
+            print ("select b," + " " + "resid" + " " + str(p[i].split()[2][3:6])+ " " +" and " +"name CA")
+            print ("show spheres, a")
+            print ("show spheres, b_")
+            print ("label a, oneletter+resi")
+            print ("label b_, oneletter+resi")
+            print("distance"+ " d2 , a, b_")
+            print("hide labels," " d2")
+            print("set dash_color, blue, d2")
+        
+        print("\n")
+
---
+++ hbonding_network.sh
@@ -0,0 +1,18 @@
+# This script will create a input file for pymol to visualize H-bonding network in a protein
+
+
+
+python Hb-details.py -f1 hbonds-details1.dat -f2 hbonds-details2.dat -c 10  > HB_details1.dat
+
+sed "s/-Main/     /g" HB_details1.dat > HB2.dat
+sed -i "s/-Side/     /g" HB2.dat
+sed -i "s/--/  /g" HB2.dat
+sed -i "s/CYSG/CGR/g" HB2.dat
+
+
+python get_data.py > hb1.pml
+
+rm -rf HB2.dat
+ 
+
+
